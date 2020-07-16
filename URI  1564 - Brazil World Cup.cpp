0fe9ddf132f238961a795872f834a70a84f91935
shakib75bd <59929433+shakib75bd@users.noreply.github.com>
2020-07-16 17:47:01 +0600

    Create URI  1564 - Brazil World Cup.cpp

---
+++ URI  1564 - Brazil World Cup.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long T;
+    while(cin >> T)
+    {
+        if (T==0) cout << "vai ter copa!\n";
+        else cout << "vai ter duas!\n";
+    }
+    
+    return 0;
+
+}
