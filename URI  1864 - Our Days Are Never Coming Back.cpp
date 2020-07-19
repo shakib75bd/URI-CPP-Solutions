3e81b03f046fc1ec5dfa5f4bd462b6f01bdb9baf
shakib75bd <59929433+shakib75bd@users.noreply.github.com>
2020-07-19 23:17:20 +0600

    Create URI  1864 - Our Days Are Never Coming Back.cpp

---
+++ URI  1864 - Our Days Are Never Coming Back.cpp
@@ -0,0 +1,23 @@
+//           ––=‹‹BISMILLAHITR RAHMANIR RAHIM››=––
+//                          (❁´◡`❁)
+//  Problem name: URI 1864 - Our Days Are Never Coming Back
+//  Lang: C++
+//  ©: _Shakib Hossen Shanto   _ CSE Dept__Begum Rokeya University,Rangpur
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a;
+    cin >> a;
+    string s = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    
+    for(int i=0;i<a;i++)
+    {
+        cout<<s[i];
+    }
+    cout<<endl;
+    
+}
