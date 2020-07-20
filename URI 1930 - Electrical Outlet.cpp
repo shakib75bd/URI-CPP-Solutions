4c42dc87093735568fe8263adc53814f170f2329
shakib75bd <59929433+shakib75bd@users.noreply.github.com>
2020-07-20 17:12:38 +0600

    Create URI 1930 - Electrical Outlet.cpp

---
+++ URI 1930 - Electrical Outlet.cpp
@@ -0,0 +1,19 @@
+//           ––=‹‹BISMILLAHITR RAHMANIR RAHIM››=––
+//                          (❁´◡`❁)
+//  Problem name: URI 1930 - Electrical Outlet
+//  Lang: C++
+//  ©: _Shakib Hossen Shanto   _ CSE Dept__Begum Rokeya University,Rangpur
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long a,b,c,d;
+    cin >>a >>b >>c >>d;
+
+    cout << a + (b-1) + (c-1) + (d-1) <<endl;
+    return 0;
+    
+}
