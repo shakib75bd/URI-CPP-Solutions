0ac1198cd617292ac6d9ae34235931f0fa3eccc4
shakib75bd <59929433+shakib75bd@users.noreply.github.com>
2020-07-22 01:23:50 +0600

    Create URI  1933 - Tri-du.cpp

---
+++ URI  1933 - Tri-du.cpp
@@ -0,0 +1,18 @@
+//           ––=‹‹BISMILLAHITR RAHMANIR RAHIM››=––
+//  Problem name: URI 1933 - tri du
+//  Lang: C++
+//  ©: _Shakib Hossen Shanto   _ CSE Dept__Begum Rokeya University,Rangpur
+
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    long long a,b;
+    cin >> a >> b;
+    long long ans = max(a,b);
+    cout<< ans <<endl;
+
+    return 0;
+}
