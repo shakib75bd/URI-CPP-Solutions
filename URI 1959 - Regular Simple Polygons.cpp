7fe43b9e3cb25e34add2c63a14941eb2fc28a592
shakib75bd <59929433+shakib75bd@users.noreply.github.com>
2020-07-23 21:19:35 +0600

    Create URI 1959 - Regular Simple Polygons.cpp

---
+++ URI 1959 - Regular Simple Polygons.cpp
@@ -0,0 +1,18 @@
+//           ––=‹‹BISMILLAHITR RAHMANIR RAHIM››=––
+//  Problem name: URI Online Judge | 1959 - Regular Simple Polygons
+//  Lang: C++
+//  ©: _Shakib Hossen Shanto   _ CSE Dept__Begum Rokeya University,Rangpur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ll a,b;
+    cin >> a >> b;
+    cout << a*b <<endl;
+
+    return 0;
+}
