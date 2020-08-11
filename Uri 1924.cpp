24002190fa491709b3f83af1354f32ddd099137c
shakib75bd <59929433+shakib75bd@users.noreply.github.com>
2020-08-11 20:23:20 +0600

    Create Uri 1924.cpp

---
+++ Uri 1924.cpp
@@ -0,0 +1,27 @@
+//           ––=‹‹BISMILLAHITR RAHMANIR RAHIM››=––
+//  Problem name:  Uri 1924
+//  Lang: C++
+//  ©: _Shakib Hossen Shanto   _ CSE Dept__Begum Rokeya University,Rangpur
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define uns unsigned
+#define ll long long
+#define dl double
+#define st string
+#define ch char
+
+int main()
+{
+      int n;
+      cin >> n;
+      while(n--)
+      {
+          st s;
+          cin >> s;
+      }
+      cout<<"Ciencia da Computacao\n";
+
+return 0; 
+}
